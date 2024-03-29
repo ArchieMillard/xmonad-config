bcb81623f7ad63141ce5787b07387aefd80154d6
Archie <84399154+ArchieMillard@users.noreply.github.com>
2024-03-29 14:18:01 +0000

    Update xmonad.hs: changed border colour.

--- configs/xmonad/xmonad.hs
+++ configs/xmonad/xmonad.hs
@@ -54,7 +54,7 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#000000"
+myNormalBorderColor  = "#363636"
 myFocusedBorderColor = "#FFFFFF"
 
 ------------------------------------------------------------------------
